b55fa69590d6c5ad4eb974ae17441aede0dd6f12
mmilton <murray.l.milton@outlook.com>
2024-02-06 12:10:30 +0100

    made revisions to the README.md, created models and added the logic for the poller

--- service/poll/poller.py
+++ service/poll/poller.py
@@ -12,15 +12,22 @@ django.setup()
 # Import models from service_rest, here. Ignore vs-code error hinting
 # from service_rest.models import Something
 
+from service_rest.models import AutomobileVO
+
+API_INVENTORY = "http//project-beta-inventory-api-1:8000/api/automobiles/"
 
 def poll():
     while True:
         print('Service poller polling for data')
         try:
-            # Write your polling logic, here
-            # Do not copy entire file
-            pass
-        
+            response = response.get(API_INVENTORY)
+            content = json.loads(response.content)
+            for auto in content["autos"]:
+                AutomobileVO.objects.update_or_create(vin=auto["vin"])
+
+            #Test for printing the Value Object within the poller
+            test_automobile = AutomobileVO.objects.all()
+            print(f"Automobile Fetched:{test_automobile}")
         except Exception as e:
             print(e, file=sys.stderr)
 
